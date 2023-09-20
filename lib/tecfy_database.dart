796efb31442467a820f482de41070922848df91d
mahmoudelgharabawi <m.elgharabawi@tecfy.co>
2023-09-20 15:32:12 +0300

    remove un wanted dependencies

--- lib/tecfy_database.dart
+++ lib/tecfy_database.dart
@@ -6,9 +6,7 @@ import 'dart:io';
 
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:collection/collection.dart';
